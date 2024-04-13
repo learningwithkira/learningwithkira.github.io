5888efdc1c3aba4db00b08f3230d4c5a10456e61
Learning With Kira <166956416+learningwithkira@users.noreply.github.com>
2024-04-14 03:08:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+setTimeout(function () {
+  window.location.href = "https://www.youtube.com/@LearningWithKira"; // Replace this URL with your desired redirect URL
+}, 1000); // 3000 milliseconds = 3 seconds
